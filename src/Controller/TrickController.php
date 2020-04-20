439dc48044679627bbcbbd197b4e9ba31d66bebc
Magali Rézeau <magalirezeau@gmail.com>
2020-04-20 21:59:35 +0200

    adding Trick, Picture and Video forms

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -3,13 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Trick;
+use App\Entity\Picture;
+use App\Form\TrickType;
 use App\Repository\TrickRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrickController extends AbstractController
 {
+    /**
+     * @Route("/trick/create", name="trick_create")
+     * 
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $entityManagerInterface)
+    {
+        $trick = new Trick();
+       
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+        return $this->render('trick/create.html.twig', [
+            'controller_name' => 'TrickController',
+            
+            'form' => $form->createView()
+        ]);
+    }
+
     /**
      * @Route("/trick/{slug}", name="trick_show")
      * 
@@ -22,4 +44,6 @@ class TrickController extends AbstractController
             'trick' => $trick
         ]);
     }
+
+    
 }
---
+++ src/Form/TrickType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trick;
+use App\Form\VideoType;
+use App\Entity\Category;
+use App\Form\PictureType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description', TextareaType::class)
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('pictures', CollectionType::class, [
+                'entry_type'   		=> PictureType::class
+               
+            ])
+            ->add('videos', CollectionType::class, [
+                'entry_type'   		=> VideoType::class
+               
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
