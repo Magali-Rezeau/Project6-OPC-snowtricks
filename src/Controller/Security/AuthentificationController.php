a00250c262439c1b60bac2a88673fa4579e1b058
Magali Rézeau <magalirezeau@gmail.com>
2020-05-20 15:19:04 +0200

    secure route

--- src/Controller/Security/AuthentificationController.php
+++ src/Controller/Security/AuthentificationController.php
@@ -2,13 +2,10 @@
 
 namespace App\Controller\Security;
 
-use App\Entity\User;
-use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
 
 class AuthentificationController extends AbstractController
 {
